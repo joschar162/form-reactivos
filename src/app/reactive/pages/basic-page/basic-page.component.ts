ba9ca9eac06cc110a6639e70ccae722b4b062a3c
josdev <chaqquererj@gmail.com>
2025-11-06 16:51:21 -0500

    validando formulario - reactive basico

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { Onclick } from '../../../../../../02-gifs-app/src/app/gifs/interfaces/giphy.interfaces';
 import {
   FormBuilder,
   FormControl,
@@ -16,10 +17,10 @@ import {
 export class BasicPageComponent {
   formBuilder = inject(FormBuilder);
 
-  myForm = this.formBuilder.group({
+  myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
-    inStorage: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   // myForm = new FormGroup({
@@ -27,4 +28,43 @@ export class BasicPageComponent {
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // });
+
+  isValidField(fieldName: string): boolean | null {
+    return (
+      !!this.myForm.controls[fieldName].errors &&
+      this.myForm.controls[fieldName].touched
+    );
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es obligatorio.';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `El valor mínimo es ${errors['min'].min}.`;
+      }
+    }
+
+    return null;
+  }
+
+  OnSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
 }
