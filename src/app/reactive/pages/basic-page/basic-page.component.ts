88fb998cf9c9f1f42c517ce5b7fb0e03fd110c5b
josdev <chaqquererj@gmail.com>
2025-11-06 12:09:03 -0500

    formGroup, formControl, formBuilder

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  formBuilder = inject(FormBuilder);
+
+  myForm = this.formBuilder.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+}
