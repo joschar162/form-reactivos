32e3eb8efaae71032dac764fef5db8b1316e0a6b
josdev <chaqquererj@gmail.com>
2025-11-06 17:17:01 -0500

    optimizando validacion usando una clase utilitaria y finalizando mosulo de reactive - basico

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,5 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Onclick } from '../../../../../../02-gifs-app/src/app/gifs/interfaces/giphy.interfaces';
 import {
   FormBuilder,
   FormControl,
@@ -8,6 +7,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -17,42 +17,13 @@ import {
 export class BasicPageComponent {
   formBuilder = inject(FormBuilder);
 
+  formUtils = FormUtils;
+
   myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
-
-  // myForm = new FormGroup({
-  //   name: new FormControl(''),
-  //   price: new FormControl(0),
-  //   inStorage: new FormControl(0),
-  // });
-
-  isValidField(fieldName: string): boolean | null {
-    return (
-      !!this.myForm.controls[fieldName].errors &&
-      this.myForm.controls[fieldName].touched
-    );
-  }
-
-  getFieldError(fieldName: string): string | null {
-    if (!this.myForm.controls[fieldName]) return null;
-
-    const errors = this.myForm.controls[fieldName].errors || {};
-    for (const key of Object.keys(errors)) {
-      switch (key) {
-        case 'required':
-          return 'Este campo es obligatorio.';
-        case 'minlength':
-          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
-        case 'min':
-          return `El valor mínimo es ${errors['min'].min}.`;
-      }
-    }
-
-    return null;
-  }
 
   OnSave() {
     if (this.myForm.invalid) {
