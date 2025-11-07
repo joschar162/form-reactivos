94e19a7436200caacf2069bde7a20cb5efff92e9
josdev <chaqquererj@gmail.com>
2025-11-07 13:02:26 -0500

    Switches, Checks y Radio buttons, completado reactive - switches

--- src/app/reactive/pages/switches-pages/switches-pages.component.ts
+++ src/app/reactive/pages/switches-pages/switches-pages.component.ts
@@ -1,9 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-pages',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-pages.component.html',
 })
-export class SwitchesPagesComponent {}
+export class SwitchesPagesComponent {
+  private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
