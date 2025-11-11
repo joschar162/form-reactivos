d666cd590b0e9f852726d6fd7b6606d9e6517969
josdev <chaqquererj@gmail.com>
2025-11-11 12:45:44 -0500

    completando selector de paises

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../service/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -15,11 +16,40 @@ export class CountryPageComponent {
   countryService = inject(CountryService);
   regions = signal(this.countryService.getRegions);
   countryByRegion = signal<Country[]>([]);
-  borders = signal<Country[]>([]);
+  border = signal<Country[]>([]);
 
   myForm = this.fb.group({
     region: ['', [Validators.required]],
     country: ['', [Validators.required]],
-    borders: ['', [Validators.required]],
+    border: ['', [Validators.required]],
   });
+
+  onFormChange = effect((onCleanUp) => {
+    const regionSubscription = this.onRegionChange();
+
+    onCleanUp(() => {
+      regionSubscription.unsubscribe();
+      console.log('Limpiado');
+    });
+  });
+
+  onRegionChange() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.border.set([]);
+          this.countryByRegion.set([]);
+        }),
+        switchMap((region) =>
+          this.countryService.getCountriesByRegion(region ?? '')
+        )
+      )
+      .subscribe((countries) => {
+        console.log({ countries });
+        this.countryByRegion.set(countries);
+      });
+  }
 }
